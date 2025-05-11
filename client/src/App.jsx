46abf5148c4c76a091896b4b43be85f75c006757
Divy Patel <divy4it@gmail.com>
2025-05-11 22:17:59 +0530

    page number added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quotes, setQuotes] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [skip, setSkip] = useState(0);
 
+  useEffect(() => {
+    fetch('https://dummyjson.com/quotes')
+      .then(res => res.json())
+      .then(data => setTotal(data.total))
+  })
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/quotes?limit=10&skip=${skip}`)
+        .then(res => res.json())
+        .then(data => setQuotes([...data.quotes.map(q => q.quote)]) )
+      
+  }, [skip])
+  
+  const handlePage = (pageNumber)=>
+  {
+    setSkip((pageNumber-1)*10)
+  }
+  const pagecount = Math.ceil(total/10); 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+
+        <div>
+          <ol>
+            {quotes.map(quote => <li key={quote}>{quote}</li>)}
+          </ol>
+        </div>
+        {Array.from({length:pagecount},(_,i)=>
+          (<button key={i+1} onClick={()=>handlePage(i+1)}>
+            {i+1}</button>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

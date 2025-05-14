8cceb50d1555fcb96e4094750a5ba6e35628b429
Divy Patel <divy4it@gmail.com>
2025-05-14 18:47:42 +0530

    drop down limit created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Quotes from './Mycomponent/quotes';
 
 function App() {
-  const [quotes, setQuotes] = useState([]);
-  const [total, setTotal] = useState(0);
-  const [skip, setSkip] = useState(0);
-
-  useEffect(() => {
-    fetch('https://dummyjson.com/quotes')
-      .then(res => res.json())
-      .then(data => setTotal(data.total))
-  })
-
-  useEffect(() => {
-    fetch(`https://dummyjson.com/quotes?limit=10&skip=${skip}`)
-        .then(res => res.json())
-        .then(data => setQuotes([...data.quotes.map(q => q.quote)]) )
-      
-  }, [skip])
-  
-  const handlePage = (pageNumber)=>
-  {
-    setSkip((pageNumber-1)*10)
-  }
-  const pagecount = Math.ceil(total/10); 
+   
   return (
     <>
-      <div className='container'>
-
-        <div>
-          <ol>
-            {quotes.map(quote => <li key={quote}>{quote}</li>)}
-          </ol>
-        </div>
-        {Array.from({length:pagecount},(_,i)=>
-          (<button key={i+1} onClick={()=>handlePage(i+1)}>
-            {i+1}</button>
-          ))}
-      </div>
+    <Quotes/>
     </>
   )
 }
---
+++ client/src/mycomponent/Quotes.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+
+function Quotes() {
+    const [quotes, setQuotes] = useState([{}]);
+    const [total, setTotal] = useState(0);
+    const [skip, setSkip] = useState(0);
+    const [currentPage, setcurrentPage] = useState(1)
+    const [pageitem, setPageitem] = useState(10)
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/quotes')
+            .then(res => res.json())
+            .then(data => setTotal(data.total))
+    })
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/quotes?limit=${pageitem}&skip=${skip}`)
+            .then(res => res.json())
+            .then(data => setQuotes([...data.quotes]))
+
+    }, [skip,pageitem])
+
+    const handlePage = (pageNumber) => {
+        setSkip((pageNumber - 1) * pageitem)
+        setcurrentPage(pageNumber)
+    }
+    const pagecount = Math.ceil(total / Number(pageitem));
+    
+    const start = Math.floor((currentPage-1)/10*10+1)
+    const end = Math.min((start+9),pagecount)
+    const visiblePages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    return (
+        <>
+        <header style={{display: "flex", justifyContent: "flex-end", padding: "10px" }}>
+            <nav>
+                <label htmlFor="limit">Limit: </label>
+                <select name="limit" id="limit" value={pageitem} onChange={(e)=>setPageitem(Number(e.target.value))}>
+                <option value="10" >10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                </select>
+            </nav>
+        </header>
+            <div className='container'>
+
+                <div>
+                    <ul>
+                        {quotes.map(quote => <li key={quote.id}>{quote.id}. {quote.quote}</li>)}
+                    </ul>
+                </div>
+                <div className='button-array'>
+                    {start>1 && (<button onClick={()=>handlePage(start>10?start-10:start-1)}>prev</button>)}
+
+                    {visiblePages.map((page)=>
+                    (<button key={page} onClick={() => handlePage(page)}>
+                        {page}</button>
+                    ))}
+
+                    {end<pagecount && (<button onClick={()=>handlePage(end+1)}>next</button>)}
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Quotes

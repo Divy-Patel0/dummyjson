0bf57cb0d92abda99cf5408b3f590f2a6b96bd62
Divy Patel <divy4it@gmail.com>
2025-05-14 19:18:58 +0530

    change pageItem to limit

--- client/src/mycomponent/Quotes.jsx
+++ client/src/mycomponent/Quotes.jsx
@@ -5,42 +5,49 @@ function Quotes() {
     const [total, setTotal] = useState(0);
     const [skip, setSkip] = useState(0);
     const [currentPage, setcurrentPage] = useState(1)
-    const [pageitem, setPageitem] = useState(10)
+    const [limit, setLimit] = useState(10)
 
     useEffect(() => {
         fetch('https://dummyjson.com/quotes')
             .then(res => res.json())
             .then(data => setTotal(data.total))
-    })
+    }, [])
 
     useEffect(() => {
         fetch(`https://dummyjson.com/quotes?limit=${pageitem}&skip=${skip}`)
             .then(res => res.json())
             .then(data => setQuotes([...data.quotes]))
 
-    }, [skip,pageitem])
+    }, [skip, limit])
 
     const handlePage = (pageNumber) => {
-        setSkip((pageNumber - 1) * pageitem)
+        setSkip((pageNumber - 1) * limit)
         setcurrentPage(pageNumber)
     }
-    const pagecount = Math.ceil(total / Number(pageitem));
-    
-    const start = Math.floor((currentPage-1)/10*10+1)
-    const end = Math.min((start+9),pagecount)
+    const pagecount = Math.ceil(total / limit);
+
+    const start = Math.floor((currentPage - 1) / 10 * 10 + 1)
+    const end = Math.min((start + 9), pagecount)
     const visiblePages = Array.from({ length: end - start + 1 }, (_, i) => start + i);
     return (
         <>
-        <header style={{display: "flex", justifyContent: "flex-end", padding: "10px" }}>
-            <nav>
-                <label htmlFor="limit">Limit: </label>
-                <select name="limit" id="limit" value={pageitem} onChange={(e)=>{setPageitem(Number(e.target.value)); setcurrentPage(1); setSkip(0)}}>
-                <option value="10" >10</option>
-                <option value="25">25</option>
-                <option value="50">50</option>
-                </select>
-            </nav>
-        </header>
+            <header style={{ display: "flex", justifyContent: "flex-end", padding: "10px" }}>
+                <nav>
+                    <label htmlFor="limit">Limit: </label>
+                    <select
+                        name="limit"
+                        id="limit" value={pageitem} 
+                        onChange={(e) => 
+                        { 
+                            setLimit(Number(e.target.value)); 
+                            setcurrentPage(1); 
+                            setSkip(0) }}>
+                        <option value="10" >10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                    </select>
+                </nav>
+            </header>
             <div className='container'>
 
                 <div>
@@ -49,14 +56,14 @@ function Quotes() {
                     </ul>
                 </div>
                 <div className='button-array'>
-                    {start>1 && (<button onClick={()=>handlePage(start>10?start-10:start-1)}>prev</button>)}
+                    {start > 1 && (<button onClick={() => handlePage(start > 10 ? start - 10 : start - 1)}>prev</button>)}
 
-                    {visiblePages.map((page)=>
+                    {visiblePages.map((page) =>
                     (<button key={page} onClick={() => handlePage(page)}>
                         {page}</button>
                     ))}
 
-                    {end<pagecount && (<button onClick={()=>handlePage(end+1)}>next</button>)}
+                    {end < pagecount && (<button onClick={() => handlePage(end + 1)}>next</button>)}
                 </div>
 
             </div>
